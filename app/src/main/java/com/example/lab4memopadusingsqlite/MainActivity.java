a5ef1dad1fd112ca0da70a9da3d5a06445906c1c
Austin Ogletree <ogletree.austin@yahoo.com>
2023-03-09 14:15:32 -0600

    Working on Graphic Interface.

--- app/src/main/java/com/example/lab4memopadusingsqlite/MainActivity.java
+++ app/src/main/java/com/example/lab4memopadusingsqlite/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.lab4memopadusingsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.lab4memopadusingsqlite.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
     }
 }
